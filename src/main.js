9fd8e7c94a2a3a98c88220cfa2a3619736839607
AungKaungMo <moea74759@gmail.com>
2024-10-21 10:06:01 +0630

    testing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createHead } from '@vueuse/head';
 // import { ViteSSG } from 'vite-ssg';
 
-// const app = createApp(App);
-// app.mount('#app')
+const app = createApp(App);
+const head = createHead();
+app.use(router);
+app.use(head)
+app.mount('#app')
 // export const createApp = ViteSSG(
 //     App,
 //     {routes},
 // )
 
 
-import { ViteSSG } from 'vite-ssg';
-import App from './App.vue';
-import router from './router'; // Make sure to import your router
+// import { ViteSSG } from 'vite-ssg';
+// import App from './App.vue';
+// import router from './router'; // Make sure to import your router
 
-export const createApp = ViteSSG(
-  App,
-  { routes: router.options.routes }, // Pass routes from your Vue Router
-  ({ app }) => {
-    // No need to mount the app here; ViteSSG handles it
-  }
-);
+// export const createApp = ViteSSG(
+//   App,
+//   { routes: router.options.routes }, // Pass routes from your Vue Router
+//   ({ app }) => {
+//     // No need to mount the app here; ViteSSG handles it
+//   }
+// );
